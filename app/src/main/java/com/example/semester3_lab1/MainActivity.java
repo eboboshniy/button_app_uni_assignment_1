162e84f4e53ee560f0bea4db1c72c12b1f931781
eboboshniy <abdukahharovsultan7@gmail.com>
2025-09-25 17:57:15 +0300

    Add Button and TextView with click functionality

--- app/src/main/java/com/example/semester3_lab1/MainActivity.java
+++ app/src/main/java/com/example/semester3_lab1/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.semester3_lab1;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        textView = findViewById(R.id.textView);
+        button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Button was clicked!");
+            }
         });
     }
 }
